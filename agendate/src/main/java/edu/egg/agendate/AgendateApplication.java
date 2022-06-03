08b251bacb2238a86dd69b21df40da5163f6009d
Rosa Rivas <rivas-rossy@hotmail.com.ar>
2022-06-02 21:51:56 -0300

    primer commit: creacion de entidades

---
+++ agendate/src/main/java/edu/egg/agendate/AgendateApplication.java
@@ -0,0 +1,13 @@
+package edu.egg.agendate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendateApplication.class, args);
+	}
+
+}
---
+++ agendate/src/test/java/edu/egg/agendate/AgendateApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.egg.agendate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
