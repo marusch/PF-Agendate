683fa0ebd92323650e265abdb2cf0b3c44d6c4ee
Fernando Silvero <ferpolasebas@gmail.com>
2022-06-14 18:51:38 -0300

    encriptacion contraseña clientes

--- agendate/src/main/java/edu/egg/agendate/servicios/ClienteServicio.java
+++ agendate/src/main/java/edu/egg/agendate/servicios/ClienteServicio.java
@@ -5,25 +5,32 @@ import edu.egg.agendate.repositorios.ClienteRepositorio;
 import edu.egg.agendate.roles.Rol;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClienteServicio {
-    
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
     private ClienteRepositorio clienteRepositorio;
-    
+
     @Transactional
     public void crear(Cliente clienteDto) {
-        if (clienteRepositorio.existsByEmail(clienteDto.getEmail()))
+        if (clienteRepositorio.existsByEmail(clienteDto.getEmail())) {
             throw new IllegalArgumentException("Ya existe un cliente con ese nombre");
+        }
 
         Cliente cliente = new Cliente();
-        
+
         cliente.setId(clienteDto.getId());
         cliente.setEmail(clienteDto.getEmail());
-        cliente.setContraseña(clienteDto.getContraseña());
+        //cliente.setContraseña(clienteDto.getContraseña());
+        //cliente.setConfirmarContraseña(clienteDto.getConfirmarContraseña());
+        cliente.setContraseña(passwordEncoder.encode(clienteDto.getContraseña()));
         cliente.setConfirmarContraseña(clienteDto.getConfirmarContraseña());
         cliente.setRol(Rol.USER);
         cliente.setNombre(clienteDto.getNombre());
@@ -57,5 +64,5 @@ public class ClienteServicio {
     public void borrarPorId(Long id) {
         clienteRepositorio.deleteById(id);
     }
-    
+
 }
