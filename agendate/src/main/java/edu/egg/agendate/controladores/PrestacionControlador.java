e47442c279b7c660dba8c908f2ba3159587bd9f0
Rosa Rivas <rivas-rossy@hotmail.com.ar>
2022-06-17 20:07:02 -0300

    se agrega el estilo del index

--- agendate/src/main/java/edu/egg/agendate/controladores/PrestacionControlador.java
+++ agendate/src/main/java/edu/egg/agendate/controladores/PrestacionControlador.java
@@ -1,11 +1,14 @@
-/*
+
 package edu.egg.agendate.controladores;
 
+import edu.egg.agendate.entidades.Prestacion;
 import edu.egg.agendate.servicios.PrestacionServicio;
+import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,24 +16,19 @@ import org.springframework.web.servlet.support.RequestContextUtils;
 
 
 @Controller
-//@RequestMapping("")
+@RequestMapping("/prestaciones")
 public class PrestacionControlador {
-  /*  
+   
   @Autowired
     private PrestacionServicio prestacionServicio;
   
      //tabla para mostrar todas las prestaciones
-    @GetMapping
-    public ModelAndView mostrarPrestaciones(HttpServletRequest request) {
-        ModelAndView mav = new ModelAndView("");
-        Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
 
-        if (inputFlashMap != null) mav.addObject("success", inputFlashMap.get("success"));
-
-        mav.addObject("prestaciones", prestacionServicio.mostrarTodos());
-        return mav;
+    @GetMapping("/lista-prestaciones")
+    public String listarTurnos(Model modelo) {
+        List<Prestacion> prestacion = prestacionServicio.mostrarTodos();
+        modelo.addAttribute("prestacion", prestacion);
+        return "lista-prestaciones";
     }
-
     
 }
-*/
